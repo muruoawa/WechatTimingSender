be5e4b13a9d8dc594754f01d1f7dcc761edfc104
muruoawa <97142912+muruoawa@users.noreply.github.com>
2023-05-20 21:45:42 +0800

    复制粘贴 func.py
    我不知道这是什么 但是chatgpt3.5给我改了这些 然后我就Ctrl c + Ctrl v了

--- func.py
+++ func.py
@@ -42,6 +42,60 @@ def gP():
 
 #验证输入的时间是否正确(格式应为: 年/月/日/时/分/秒(年月日可省略 时分秒若早于当前时间则自动转为明天),输入框变量为date)
 
+#导入基本库
+import pyautogui
+import time
+from datetime import datetime
+import threading
+from pynput import mouse
+
+prompts = '点击获取基本位置后输入你需要执行的时间 然后点击执行来准备执行操作'
+#文本框内容转换
+def PChange(pnum):
+    global prompts
+    prompt_dict = {
+        100: '点击获取基本位置后输入你需要执行的时间，然后点击执行来准备执行操作',
+        101: '请点击你的鼠标左键到微信的输入框上',
+        102: '请点击你的鼠标左键到微信的发送按钮上'
+    }
+    prompts = prompt_dict.get(pnum, '点击获取基本位置后输入你需要执行的时间，然后点击执行来准备执行操作')
+
+#获取基本鼠标位置
+sendPx = 0
+sendPy = 0
+inputPx = 0
+inputPy = 0
+def gP():
+    global sendPx, sendPy, inputPx, inputPy
+
+    def on_click(x, y, button, pressed):
+        if pressed:
+            PChange(101)
+            if prompts == '请点击你的鼠标左键到微信的输入框上':
+                nonlocal inputPx, inputPy
+                inputPx, inputPy = x, y
+            PChange(102)
+            if prompts == '请点击你的鼠标左键到微信的发送按钮上':
+                nonlocal sendPx, sendPy
+                sendPx, sendPy = x, y
+            PChange(100)
+
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join()
+
+    print(inputPx, inputPy, sendPx, sendPy)
+
+
+
+#验证输入的时间是否正确(格式应为: 年/月/日/时/分/秒(年月日可省略 时分秒若早于当前时间则自动转为明天),输入框变量为date)
+
+
+
+    #按下执行后执行代码
+    def execute_code():
+        #需要执行的内容 在指定时间复制内容到剪切板 粘贴到输入框(inputPx, inputPy)中 然后左键单击发送(sendPx, sendPy)
+        print("执行代码")
+
 
 
     #按下执行后执行代码
